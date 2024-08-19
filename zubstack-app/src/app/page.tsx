f79093cb50f616e8ea0036fd12f2e52c25401c8d
geovlunaa <lunacarlos.dev@gmail.com>
2024-08-18 19:56:09 -0500

    app: main layout feed

--- zubstack-app/src/app/page.tsx
+++ zubstack-app/src/app/page.tsx
@@ -2,19 +2,21 @@ import { BlogItem } from "@/components/BlogItem";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between">
-        <p className="text-[24pt] text-center items-center line-clamp-5 tracking-widest h-14 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 before:fixed before:top-44 before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:z-[-1] before:content-[''] after:fixed after:z-[-1] after:h-[180px] after:w-full after:top-20 after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className=" w-full max-w-5xl items-center justify-between">
+        <p className="text-[24pt] text-center items-center line-clamp-5 tracking-widest h-14 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit z-10">
           Zub
-          <p className="font-black">Stack</p>
+          <span className="font-black">Stack</span>
         </p>
       </div>
 
-      <div className="relative z-[-1] flex w-full justify-center place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <div className="flex flex-col space-y-4 py-20 ">
-          {[1, 2, 3, 4, 5].map((i, v) => (
-            <BlogItem key={v} />
-          ))}
+      <div className="flex w-full justify-center place-items-center pt-4">
+        <div className="flex flex-row w-full justify-center">
+          <div className="flex flex-col space-y-4 py-20 ">
+            {[1, 2, 3, 4, 5].map((_, v) => (
+              <BlogItem key={v} />
+            ))}
+          </div>
         </div>
       </div>
 
--- zubstack-app/src/components/BlogItem.tsx
+++ zubstack-app/src/components/BlogItem.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { FC } from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { IoMdArrowRoundUp } from "react-icons/io";
 
 type BlogItemProps = {};
 
 export const BlogItem: FC<BlogItemProps> = () => {
   return (
-    <div className="flex flex-row w-[60vw] bg-black space-x-2 border-neutral-800 p-4 border rounded-lg">
+    <div className="flex flex-row w-[60vw] space-x-2 border-neutral-800 p-5 border rounded-lg bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit ">
       <div>
-        <div className="rounded-full h-10 flex w-10 font-bold border border-neutral-800 items-center justify-center">
+        <div className="rounded-full bg-zinc-800 h-10 flex w-10 font-bold border border-neutral-800 items-center justify-center">
           A
         </div>
       </div>
@@ -18,7 +21,7 @@ export const BlogItem: FC<BlogItemProps> = () => {
           </p>
         </div>
 
-        <article className="pt-2">
+        <article className="py-2">
           <h4 className="font-bold text-[24px] ">
             Native App Development is Dead
           </h4>
@@ -28,8 +31,23 @@ export const BlogItem: FC<BlogItemProps> = () => {
             native.
           </div>
         </article>
-        <div className="flex flex-row space-x-4">
-          <div></div>
+        <div className="flex flex-row space-x-4 pt-2 text-[#D1CDC7] duration-1000 transition-all">
+          <button
+            type="button"
+            className="flex flex-row flex-wrap space-x-2 items-center font-mono text-center border border-neutral-800 px-2 py-1 rounded-full hover:text-white"
+          >
+            <IoMdArrowRoundUp />
+            <p className="text-center"> 45k </p>
+          </button>
+          <div className="items-center flex flex-1 justify-end ">
+            <Link
+              href={""}
+              className="flex flex-row space-x-4 items-center hover:space-x-6"
+            >
+              <p className="text-sm font-mono">READ MORE</p>
+              <FaArrowRightLong />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
