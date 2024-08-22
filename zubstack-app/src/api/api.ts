89d07d876505acd5e29568ba56fb60fdd27cc203
geovlunaa <lunacarlos.dev@gmail.com>
2024-08-22 16:49:16 -0500

    docker-compose ✅

--- zubstack-app/src/api/api.ts
+++ zubstack-app/src/api/api.ts
@@ -1,5 +1,8 @@
+"use client";
+
 import axios from "axios";
 
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 3000,
